b733207bbd2fa78510a9fd535be54200d9c6a19c
Abdul Rachmat <idsumoblog@gmail.com>
2026-08-22 11:17:34 +0700

    Debug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'main-edu-v25';
+const CACHE_NAME = 'main-edu-v26';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -56,7 +56,7 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW] Caching app shell v20...');
+      console.log('[SW] Caching app shell v26...');
       return cache.addAll(ASSETS_TO_CACHE).catch((err) => {
         console.warn('[SW] Cache addAll failed, caching individually:', err);
         return Promise.all(
